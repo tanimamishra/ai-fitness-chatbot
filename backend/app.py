366f06c7f6fd0f8feec5f67ad40b70cb7a038583
Isha Sharma <isha.312.sharma@gmail.com>
2026-03-17 02:45:57 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "AI Fitness Chatbot Backend Running"
+
+
+@app.route("/chat", methods=["POST"])
+def chat():
+
+    data = request.json
+    message = data.get("message")
+
+    return jsonify({
+        "reply": "Backend received message: " + message
+    })
